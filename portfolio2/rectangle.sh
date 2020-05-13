755100e0643d33e7b215b816f8fbfbc354a6b4fc
Peter Whitehead <Havanapple@github.com>
2020-05-13 11:39:52 +0000

    portfolio 2

--- portfolio2/rectangle.sh
+++ portfolio2/rectangle.sh
@@ -9,7 +9,7 @@ targetFile=rectangle.txt
 while ! [ -f "$targetFile" ]; do    # While the targetFile does not exist, do this
     if ! [[ $targetFile = 'x' ]]; then        # Allow for exit option
         echo "Could not find $targetFile"
-        read -p 'Please enter an alternate directory/file or press x to exit: ' targetFile     # Prompt for alternate directory/file
+        read -p 'Please enter an alternate directory/file or press x to exit: ' targetFile   # Prompt for alternate dir/file
     else
         exit 0
     fi
@@ -19,12 +19,12 @@ echo "Processing $targetFile..."     #Feedback for user
 # Sed initialised.
 # First line removes 1st line of document
 # Second line aims at the beginning of each line with ^ and inserts 'Name: ' before the line characters
-# Third to sixth line replaces the first commas of each line in turn with Width, Height, Area, and Colour labels
+# Third to sixth line replaces the first commas of each line in turn with Height, Width, Area, and Colour labels
 # The final line (sixth) defines the input and output files
 sed -e '1d'\
     -e 's/^/Name: /g'\
-    -e 's/,/\tWidth: /1'\
     -e 's/,/\tHeight: /1'\
+    -e 's/,/\tWidth: /1'\
     -e 's/,/\tArea: /1'\
     -e 's/,/\tColour: /1' $targetFile > rectangle_f.txt
 
